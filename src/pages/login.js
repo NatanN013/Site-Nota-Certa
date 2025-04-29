a1c2a52e1970226b349a0f29b97a2351e6e337bf
NatanN013 <Natanplays6@gmail.com>
2025-04-29 20:09:57 -0300

    Update login.js

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
-import { auth } from '../firebase';
+import { auth } from '../../firebase';
 import {
   signInWithEmailAndPassword,
   GoogleAuthProvider,
